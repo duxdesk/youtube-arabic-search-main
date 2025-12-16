daa01d261534b2532763ffea0824c99c98f3d9ac
Saad <saadfawsi@hotmail.com>
2025-12-16 20:47:24 +0100

    Resolve main.tsx merge conflict and ignore venv/

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-<<<<<<< HEAD
+ HEAD
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
-=======
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
@@ -16,6 +16,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <ErrorBoundary>
       <App />
     </ErrorBoundary>
-  </React.StrictMode>,
-);
->>>>>>> origin/main
+  </React.StrictMod >
+origin/main
